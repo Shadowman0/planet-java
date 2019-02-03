8c5faa553b16308afb2dc35ea40449cf9fd76b16
Shadowman <Shadowman0@web.de>
2019-02-03 18:02:24 +0100

    setup changed

--- rocketsSocket/src/main/java/socket/rocket/model/Simulation.java
+++ rocketsSocket/src/main/java/socket/rocket/model/Simulation.java
@@ -5,8 +5,11 @@ public class Simulation {
 
 	public Simulation() {
 		bodyList.createPlanet(50000, new Vector(0, 0), new Vector(0, 0), 100);
-		bodyList.createPlanet(10, new Vector(100, 0), new Vector(0, 100), 1);
-		bodyList.createPlanet(10, new Vector(-200, 0), new Vector(0, -25), 1);
+		bodyList.createPlanet(1, new Vector(100, 0), new Vector(0, 100), 1);
+		bodyList.createPlanet(1, new Vector(-200, 0), new Vector(0, -100), 1);
+		bodyList.createPlanet(1, new Vector(-300, 0), new Vector(0, -100), 1);
+		bodyList.createPlanet(1, new Vector(-400, 0), new Vector(0, -100), 1);
+		bodyList.createPlanet(1, new Vector(-500, 0), new Vector(0, -100), 1);
 	}
 
 }
